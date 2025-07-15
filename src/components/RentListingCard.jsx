69600865a9f3c70e7ba7d240a306a6f25e92f217
charlyyoda <cagyarko233@gmail.com>
2025-07-15 19:17:21 +0000

    rent listing card and page commit

--- src/components/RentListingCard.jsx
+++ src/components/RentListingCard.jsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router";
+import { MapPin } from "lucide-react";
+
 export default function RentListingCard() {
+
     return (
-        <>
-        <h1 className="text-4xl text-violet-900 font-extrabold flex justify-center">THIS THE RENT LISTING CARD FOR THE HOMES TO BE BOOKED OF THE WEBSITE</h1>
-        </>
+        <div className="overflow-hidden w-full sm:max-w-sm rounded-lg shadow-sm bg-white hover:shadow-lg mx-auto mt-10">
+            <img
+                src="https://plus.unsplash.com/premium_photo-1675537857054-8c7e518553af?q=80&w=854&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Room Image"
+                className="w-full h-54 sm:h-62 md:h-70 object-cover transition-transform duration-300 group-hover:scale-105"
+            />
+
+            <div className="bg-white w-[90%] mx-auto my-4">
+                <div className="flex justify-between">
+                    <h2 className="text-lg md:text-xl font-semibold text-black">Cozy Studio</h2>
+                    <p className="rounded-full bg-[#27AE60] py-1 px-3 text-white">Verified</p>
+                </div>
+                <p className="text-black font-semibold text-sm md:text-lg mt-2">
+                    Perfect for young professionals
+                </p>
+                <div className="flex mt-2 text-[#7F8C8D] font-semibold">
+                    <MapPin className="" />
+                    <p className="text-sm md:text-lg">Achimota</p>
+                </div>
+
+
+                <div className="flex items-center justify-between mt-4">
+                    <span className="text-lg md:text-2xl font-medium text-black">Ghc1,500/month</span>
+
+                    <Link to="/artandpaintings">
+                        <button className="bg-[#2980B9] font-semibold text-white text-xs md:text-sm px-3 md:px-4 py-1.5 md:py-2 rounded-lg hover:bg-[#1F618D] transition-colors">
+                            View Details
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+
     );
 }
--- src/pages/RentListings.jsx
+++ src/pages/RentListings.jsx
@@ -1,7 +1,89 @@
+import RentListingCard from "../components/RentListingCard";
+import { Search } from "lucide-react";
+
 export default function RentListing() {
+
     return (
         <>
-        <h1 className="text-4xl text-violet-900 font-extrabold flex justify-center">WELCOME TO OUR RENT LISTING PAGE FOR RENTAL WEBSITE</h1>
+            <div className="w-[94%] my-10 mx-auto">
+                <h1 className="font-medium text-4xl">Rental Listings</h1>
+                <p className="text-xl font-semibold mt-4">Find your perfect rental property</p>
+
+                <div className="overflow-hidden w-full rounded-xl border-[#a7b3b4] border-1 shadow-sm bg-white mt-10 p-6">
+                    <form action="" className="flex flex-row justify-around">
+                        <div className="flex flex-col">
+                            <label htmlFor="" className="font-medium">Location</label>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="Enter city or area"
+                                className="w-45 py-1 px-4 border-[#7F8C8D] border text-[#7F8C8D] text-lg font-semibold rounded-lg mt-2" />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="" className="font-medium">Price Range</label>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="Any Price"
+                                className="w-45 py-1 px-4 border-[#7F8C8D] border text-[#7F8C8D] text-lg font-semibold rounded-lg mt-2" />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="" className="font-medium">Room Type</label>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="Any Room"
+                                className="w-45 py-1 px-4 border-[#7F8C8D] border text-black text-lg font-semibold rounded-lg mt-2" />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <label htmlFor="" className="font-medium">Listing Type</label>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="All Listings"
+                                className="w-45 py-1 px-4 border-[#7F8C8D] border text-black text-lg font-semibold rounded-lg mt-2" />
+                        </div>
+
+                        <div className="flex flex-col">
+                            {/* <Search /> */}
+                            <button
+                                type="submit"
+                                className="bg-black text-white text-lg py-1 px-4 w-45 mt-8 rounded-lg font-semibold"
+                            >Search</button>
+                        </div>
+
+                    </form>
+                </div>
+
+
+                <div className="flex flex-row justify-between mt-10">
+                    <p className="text-lg font-medium">Showing 1-12</p>
+                    <form action="">
+                        <label htmlFor="" className="font-medium">Sort By:</label>
+                        <input
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Price: Low-High"
+                            className="w-45 py-1 px-4 border-[#7F8C8D] border text-black text-lg font-semibold rounded-lg ml-4" />
+                    </form>
+
+                </div>
+
+            
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p gap-8 justify-between w-full mx-auto">
+                    {[1, 2, 3, 4, 5, 6,].map(n => <RentListingCard key={n} />)}
+                </div>
+
+            </div>
+
         </>
     );
 }
