8d1d3cd8786aa79d2257402865d3a0b54c539833
Domatiara <michellnandara2000@gmail.com>
2025-07-26 13:14:53 +0000

    For Owner Changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,7 +103,7 @@ export default function Navbar() {
                                     </div>
                                 </div> */}
                             </div>
-                            <Link to='/owner-listing-form' className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-800 transition-colors">
+                            <Link to='/owner-dashboard' className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-800 transition-colors">
                                 For Owners
                                 </Link>
                             <Link to='/contact' className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-800 transition-colors">
